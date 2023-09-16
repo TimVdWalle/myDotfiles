3e70283e7216d55eceb37ce80ece6772a38cb10a
Tim Vande Walle <tim@vandewalle.mobi>
2023-09-16 19:59:06 +0200

    Update output

--- resources/overview.sh
+++ resources/overview.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 php --version
+print_after_newline "print_with_newline"
 laravel --version
+print_after_newline "print_with_newline"
 composer --version
+print_after_newline "print_with_newline"
 
 print_after_newline "print_with_newline"
