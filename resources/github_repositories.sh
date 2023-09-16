c46b7324f114b773ced8715bc7d5ba84b288a61a
Tim Vande Walle <tim@vandewalle.mobi>
2023-09-17 00:15:01 +0200

    Add script to clone github repositories (wip)

---
+++ resources/github_repositories.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#CODE=$HOME/Code
+#SITES=$HOME/Herd
+#BLADE=$CODE/blade-ui-kit
+#LARAVEL=$CODE/laravel
+
+## Sites
+#git clone git@github.com:blade-ui-kit/blade-ui-kit.com.git $SITES/blade-ui-kit.com
+#git clone git@github.com:laravel/blog.laravel.com.git $SITES/blog.laravel.com
