b3e35200b3fb4f31223bdacb6d14066e483d6dec
Tim Vande Walle <tim@vandewalle.mobi>
2023-09-18 20:08:57 +0200

    Update xdebug setup: make conditional

--- resources/xdebug.sh
+++ resources/xdebug.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-brew install php
-pecl install xdebug
+ask_for_confirmation "Do you want to setup xdebug?"
+if answer_is_yes; then
+  brew install php
+  pecl install xdebug
 
-echo "Update php.ini: find correct location of php.ini in Herd UI"
-echo "See documentation on : https://herd.laravel.com/docs/1/advanced-usage/xdebug"
-echo "Press enter to continue"
-read waiting
+  echo "Xdebug setup needs brew install php as long as xdebug is not included in herd"
+  echo "Update php.ini: find correct location of php.ini in Herd UI"
+  echo "See documentation on : https://herd.laravel.com/docs/1/advanced-usage/xdebug"
+  echo "Press enter to continue"
+  read waiting
+
+  print_after_newline "print_with_newline"
+else
+    print_after_newline "print_with_newline"
+    echo "Skipping xdebug setup"
+fi;
 
 print_after_newline "print_with_newline"
+
+
+
+
+
