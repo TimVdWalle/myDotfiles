56d504756f51afbc69478863a37066275047c560
Tim Vande Walle <tim@vandewalle.mobi>
2023-09-18 19:07:09 +0200

    Update overview with fonts

--- resources/overview.sh
+++ resources/overview.sh
@@ -10,4 +10,9 @@ echo $SHELL
 print_after_newline "print_with_newline"
 #eval "$(starship init zsh)"
 
+echo "Testing fonts..."
+source ./resources/test-fonts.sh
+
+
+
 print_after_newline "print_with_newline"
