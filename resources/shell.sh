aa1a5308c2d117e8cdc1013410818cf6c044465e
Tim Vande Walle <tim@vandewalle.mobi>
2023-09-18 19:06:55 +0200

    Update shell install script

--- resources/shell.sh
+++ resources/shell.sh
@@ -1,29 +1,47 @@
 #!/bin/sh
 
-# Switch to zsh shell
+# Install zsh with brew, even though zsh is already provided by macos (installing zsh with brew creates the config file in the right place)
+brew install zsh
+
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-#brew install spaceship
 
 brew tap homebrew/cask-fonts
 brew install font-hack-nerd-font
-brew install --cask font-nerd-fonts
-brew install --cask font-fira-code
 
-brew install starship
+# Install the pure prompt : https://github.com/sindresorhus/pure
+brew install pure
 
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Install the color scheme that goes beautifully with the pure prompt
+hyper install hyper-snazzy
 
 
-#https://timonbimon.medium.com/yet-another-step-by-step-guide-for-a-better-terminal-setup-6c5e879d4c8c
+
+# write settings to .zshrc
+
+
+#brew install spaceship
+
+#brew tap homebrew/cask-fonts
+#brew install font-hack-nerd-font
+#brew install --cask font-nerd-fonts
+#brew install --cask font-fira-code
+
+#brew install starship
+
+## install oh-my-zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+##https://timonbimon.medium.com/yet-another-step-by-step-guide-for-a-better-terminal-setup-6c5e879d4c8c
 
 # Install powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
 cd ..
+rm -rf fonts/
 
-print_after_newline "print_with_newline"
+#print_after_newline "print_with_newline"#
