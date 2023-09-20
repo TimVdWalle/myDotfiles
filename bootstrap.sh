505e469ff1f22ae4a5f9ced021e331af62fdd7c9
Tim Vande Walle <timvandewalle@MBP-van-Tim.lan>
2023-09-20 21:47:29 +0200

    Add progress bar

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,55 +1,83 @@
 #!/usr/bin/env bash
 
+# bootstrap.sh: A script to set up a macOS machine for development.
+
+# Source the progress bar functions
+# This provides the show_progress and increment_step_and_show_progress functions
+source ./resources/progress_bar.sh
+
+# Display an initial message
 echo "Setting up your Mac..."
 
-# Loading helper functions
+# Initialize variables to manage the progress bar
+# total_steps: The total number of setup actions
+# current_step: The current setup action being executed
+total_steps=11
+current_step=0
+
+# Start each major setup action
+
+# Load helper functions
+increment_step_and_show_progress
 echo "Loading helper functions..."
 source ./resources/utils.sh
 source ./resources/utils-macos.sh
 
-# Setting ssh key (for github)
+# Set SSH key for GitHub
+increment_step_and_show_progress
 echo "Setting ssh key for github..."
 source ./resources/setup_github_ssh_key.sh
 
-# Installing HomeBrew
+# Install HomeBrew (Package Manager)
+increment_step_and_show_progress
 echo "Installing HomeBrew..."
 source ./resources/install_homebrew.sh
 
-# Installing tools + apps
+# Install various tools and apps via HomeBrew
+increment_step_and_show_progress
 echo "Installing tools + apps..."
 brew bundle --file ./resources/brewfile
 print_after_newline "print_with_newline"
 
-# Installing Xdebug
+# Install Xdebug (Debugging tool for PHP)
+increment_step_and_show_progress
 echo "Installing Xdebug..."
 source ./resources/install_xdebug.sh
 
-# Linking local dotfiles folder with github
+# Link local dotfiles folder with GitHub
+increment_step_and_show_progress
 echo "Linking local dotfiles folder with github..."
 source ./resources/link_github.sh
 
-# Cloning my other repositories to local
+# Clone other personal GitHub repositories
+increment_step_and_show_progress
 echo "Cloning my other repositories to local..."
 source ./resources/clone_github_repositories.sh
 
-# Installing zsh etc
+# Set up ZSH shell
+increment_step_and_show_progress
 echo "Installing zsh etc..."
 source ./resources/setup_shell.sh
 
-# Creating symlinks (folders etc, no dotfiles)
+# Create symbolic links for various folders
+increment_step_and_show_progress
 echo "Creating symlinks..."
 source ./resources/create_folder_symlinks.sh
 
-# Loading macos settings
-echo "Loading macos settings..."
+# Load macOS specific preferences/settings
+increment_step_and_show_progress
+echo "Loading macOS settings..."
 source ./resources/load_macos_preferences.sh
 
-# Symlinking dotfiles
+# Create symbolic links for dotfiles
+increment_step_and_show_progress
 echo "Symlinking dotfiles..."
 source ./resources/symlink_dotfiles.sh
 
-
-
-# Showing installation overview
+# Show an installation overview
+increment_step_and_show_progress
 echo "Showing installation overview..."
 source ./resources/overview.sh
+
+# Move to the next line after the progress bar reaches 100%
+echo ""
