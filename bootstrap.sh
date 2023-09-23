619f1cdbbcfc504983e1713e7d70969872c290d3
Tim Vande Walle <timvandewalle@MBP-van-Tim.lan>
2023-09-23 21:39:19 +0200

    Refactor step scriptnames + location

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,34 +11,34 @@ check_os
 confirm_install
 
 # Set SSH key for GitHub integration.
-run_script "Setting ssh key for github..." "./resources/setup_github_ssh_key.sh"
+run_script "Setting ssh key for github..." "./steps/setup_github_ssh_key.sh"
 
 # Install HomeBrew
-run_script "Installing HomeBrew..." "./resources/install_homebrew.sh"
+run_script "Installing HomeBrew..." "./steps/install_homebrew.sh"
 
 # Install tools and applications specified in the brewfile.
 run_script "Installing tools + apps..." "brew bundle --file ./resources/brewfile"
 
 # Install Xdebug for PHP debugging.
-run_script "Installing Xdebug..." "./resources/install_xdebug.sh"
+run_script "Installing Xdebug..." "./steps/install_xdebug.sh"
 
 # Link local dotfiles folder to GitHub.
-run_script "Linking local dotfiles folder with github..." "./resources/link_github.sh"
+run_script "Linking local dotfiles folder with github..." "./steps/link_github.sh"
 
-# Clone other required repositories.
-run_script "Cloning my other repositories to local..." "./resources/clone_github_repositories.sh"
+# Clone other repositories.
+run_script "Cloning my other repositories to local..." "./steps/clone_github_repositories.sh"
 
 # Set up the shell environment.
-run_script "Installing zsh etc..." "./resources/setup_shell.sh"
+run_script "Installing zsh etc..." "./steps/setup_shell.sh"
 
 # Create symbolic links for specified folders.
-run_script "Creating symlinks..." "./resources/create_folder_symlinks.sh"
+run_script "Creating symlinks..." "./steps/create_folder_symlinks.sh"
 
 # Load macOS-specific settings/preferences.
-run_script "Loading macos settings..." "./resources/load_macos_preferences.sh"
+run_script "Loading macos settings..." "./steps/load_macos_preferences.sh"
 
 # Create symbolic links for dotfiles.
-run_script "Symlinking dotfiles..." "./resources/symlink_dotfiles.sh"
+run_script "Symlinking dotfiles..." "./steps/symlink_dotfiles.sh"
 
 # Display installation summary or overview.
-run_script "Showing installation overview..." "./resources/show_overview.sh"
+run_script "Showing installation overview..." "./steps/show_overview.sh"
--- steps/show_overview.sh
+++ steps/show_overview.sh
@@ -11,9 +11,4 @@ echo $SHELL
 print_after_newline "print_with_newline"
 #eval "$(starship init zsh)"
 
-#echo "Testing fonts..."
-#source ./resources/test-fonts.sh
-
-
-
 print_after_newline "print_with_newline"
