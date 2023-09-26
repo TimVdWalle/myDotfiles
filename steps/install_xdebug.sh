f7727be3dd6d3591acba215b1c66a11524c31b77
Tim Vande Walle <timvandewalle@MBP-van-Tim.lan>
2023-09-26 20:51:33 +0200

    Add check for cpu architecture : intel vs m1/m2/mx

--- steps/install_xdebug.sh
+++ steps/install_xdebug.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
+ARCHITECTURE=$(uname -m)
+
 # Check if xdebug is installed
-if ! pecl list | grep -q xdebug; then
-  brew install php
-  pecl install xdebug
+if ! php -m | grep -q xdebug; then
+  # Check architecture and execute appropriate commands
+  if [ "$ARCHITECTURE" = "x86_64" ]; then
+    # Commands for Intel
+    echo "Detected Intel architecture. Running Intel-specific commands."
+    brew install php
+    pecl install xdebug
+  elif [ "$ARCHITECTURE" = "arm64" ]; then
+    # Commands for M1/M2/Mx
+    echo "Detected M1/M2/Mx architecture. Running M1/M2/Mx-specific commands."
+    # Currently, the same commands as for Intel are used. Adjust if necessary.
+    brew install php
+    pecl install xdebug
+  else
+    echo "Unknown architecture detected."
+    brew install php
+    pecl install xdebug
+  fi
 
   echo "Xdebug setup needs brew install php as long as xdebug is not included in herd"
   echo "Update php.ini: find correct location of php.ini in Herd UI"
