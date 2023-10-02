2ee83d2e05fb14a6057046a244559c3f1c49992a
Tim Vande Walle <tim@vandewalle.mobi>
2023-10-02 12:25:19 +0200

    Update macos settings script: disable all

--- steps/load_macos_preferences.sh
+++ steps/load_macos_preferences.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# Displays have separate Spaces
-# == dont make dock switch displays
-defaults write com.apple.spaces "spans-displays" -bool "false" && killall SystemUIServer
+# Only show active apps
+#defaults write com.apple.dock "static-only" -bool "true" && killall Dock
 
-# Show recent applications in Dock
-#defaults write com.apple.dock "show-recents"  -bool false
+# Do not display recent apps in the Dock
+#defaults write com.apple.dock "show-recents" -bool "false" && killall Dock
 
-# Show hidden files:
-defaults write com.apple.finder AppleShowAllFiles YES;
+#defaults write com.apple.dock persistent-apps -array
+
+#for dockItem in {/System/Applications/{"Mail","Notes","System Preferences","App Store","Preview"},/Applications/{"iTerm","Visual Studio Code"}.app; do
+#  defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>'$dockItem'</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+#done
+
 
 echo "MacOS settings preferences are loaded."
 
