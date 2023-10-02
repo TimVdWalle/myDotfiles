7fc67fdf39ad65ca9bd577aceeb2850d3ca17fa8
Tim Vande Walle <tim@vandewalle.mobi>
2023-10-02 12:26:50 +0200

    Fix bug: pecl command not found during installation script , trying with full path

--- steps/install_xdebug.sh
+++ steps/install_xdebug.sh
@@ -4,7 +4,7 @@
 ARCHITECTURE=$(uname -m)
 
 # Check if xdebug is installed
-if ! pecl list | grep -q xdebug; then
+if ! /usr/local/bin/pecl list | grep -q xdebug; then
   # Check architecture and execute appropriate commands
   if [ "$ARCHITECTURE" = "x86_64" ]; then
     # Commands for Intel
