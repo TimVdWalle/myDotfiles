0fc19a7c565fa2bcdc79b1248803a742cf04c1b0
Tim Vande Walle <tim@vandewalle.mobi>
2023-11-17 21:19:43 +0100

    ✨ Add phpstan-watcher to automatically downloaded projects

--- steps/clone_github_repositories.sh
+++ steps/clone_github_repositories.sh
@@ -2,16 +2,20 @@
 
 LARAVEL=~/Projects/Laravel/
 OTHER=~/Projects/Other/
+JS=~/Projects/JavaScript/
 
 if [ ! -e $LARAVEL ]; then
   mkdir $LARAVEL
   mkdir $OTHER
+  mkdir JS
 
   git clone git@github.com:TimVdWalle/perfume-picker.git $LARAVEL/perfume-picker
   git clone git@github.com:TimVdWalle/dart-score.git $LARAVEL/dart-score
   git clone git@github.com:TimVdWalle/text2playlist.git $LARAVEL/text2playlist
 
   git clone git@github.com:TimVdWalle/commit-verbs.git $OTHER/commit-verbs
+
+  git clone git@github.com:TimVdWalle/phpstan-watcher.git $JS/phpstan-watcher
 
   # setup git hook script(s)
   chmod +x $OTHER/commit-verbs/git-templates/hooks/prepare-commit-msg
